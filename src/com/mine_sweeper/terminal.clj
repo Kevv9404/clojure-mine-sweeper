825b2756f2e88307a9db2ea3fb4aa24bc2e0afca
kevinarroyoc <karroyocardenas@gmail.com>
2025-02-27 13:45:13 -0500

    Terminal created

---
+++ src/com/mine_sweeper/terminal.clj
@@ -0,0 +1,29 @@
+(ns com.mine-sweeper.terminal
+  (:import (java.nio.charset StandardCharsets)
+           (jdk.internal.org.jline.terminal Terminal)
+           (com.googlecode.lanterna.terminal DefaultTerminalFactory Terminal)))
+
+
+(defn terminal []
+  (.createTerminal (new DefaultTerminalFactory System/out, System/in, StandardCharsets/UTF_8)))
+
+(defn init-terminal [^Terminal t]
+  (.enterPrivateMode t)
+  (.clearScreen t))
+
+(defn move-cursor [^Terminal t x y]
+  (.setCursorPosition t x y)
+  (.flush t))
+
+(defn put-character [^Terminal t x y ^Character c]
+  (.setCursorPosition t x y)
+  (.putCharacter t c)
+  (.flush t))
+
+(defn get-next-key-press [^Terminal t]
+  (let [keyStroke (.pollInput t)]
+    (when keyStroke
+      (.getCharacter keyStroke))))
+
+
+(def t (terminal))
