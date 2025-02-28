c02a70e0eeeaace6610ce0e0fe03d3fd2d9d2422
Isabel <shakiraisabelgp1003@gmail.com>
2025-02-28 08:53:36 -0500

    Created test directory and added fulcro spec

--- test/com/mine_sweeper/model/field_test.clj
+++ test/com/mine_sweeper/model/field_test.clj
@@ -1,6 +1,7 @@
-(ns field
+(ns com.mine-sweeper.model.field-test
   (:require [clojure.test :refer :all]
-            [com.mine-sweeper.model.field :as field]))
+            [com.mine-sweeper.model.field :refer :all]
+            [fulcro-spec.core :refer [=> assertions component behavior specification when-mocking]]))
 
 
 (deftest cells-are-hidden
