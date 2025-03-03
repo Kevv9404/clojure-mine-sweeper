18eeac5b69b841344a65238a3cb8a02d525f5488
kevinarroyoc <karroyocardenas@gmail.com>
2025-03-03 18:28:49 -0500

    Playground

--- src/com/mine_sweeper/model/playground.clj
+++ src/com/mine_sweeper/model/playground.clj
@@ -1,16 +1,14 @@
 (ns com.mine-sweeper.model.playground
-  (:require [com.mine-sweeper.terminal :as terminal]
+  (:require [clojure.string :as str]
+            [com.mine-sweeper.terminal :as terminal]
             [com.mine-sweeper.model.field :as field])
   (:import (jdk.internal.org.jline.terminal Terminal)))
 
 
-(defn draw-cell [^Terminal t grid cell]
-  )
-
 (defn setup [width height n]
   (-> (field/mine-field width height)
-      (field/populate-mines n)
-      (field/initialize-mine-counts))
+    (field/populate-mines n)
+    (field/initialize-mine-counts))
   )
 
 (defn draw-grid [^Terminal t mine-field]
@@ -18,19 +16,19 @@
     (mapv (fn [row]
             (mapv (fn [cell]
                     (if (= (:cell/hidden? cell) true)
-                      (terminal/put-character t (:cell/x cell) (:cell/y cell) \X)))
-                  row))
-          grid)
-    (doseq [y (range (count grid))
-            x (range (count (first grid)))]
-      (draw-cell t grid (get-in grid [x y])))))
+                      (terminal/put-character t (:cell/x cell) (:cell/y cell) \~)
+                      (cond
+                        (= (:cell/content cell) :mine) (terminal/put-character t (:cell/x cell) (:cell/y cell) \*)
+                        (number? (:cell/content cell)) (terminal/put-character t (:cell/x cell) (:cell/y cell) (char (+ 48 (:cell/content cell))))
+                        (= (:cell/flagged? cell) true) (terminal/put-character t (:cell/x cell) (:cell/y cell) \?))))
+              row)) grid)))
 
 
+(def t (terminal/terminal))
+(draw-grid t (setup 10 10 3))
 
 (comment
-  (def t (terminal/terminal))
-  (def mine-field (field/mine-field 10 10))
-  (draw-grid t mine-field)
+
   )
 
 
