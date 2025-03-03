49d789d07fc40c5f9a5a07d3ca86914314353424
Shakira Galvan <shakira@dataico.com>
2025-03-03 10:22:04 -0500

    Added build-grid test and refactor expand

--- test/com/mine_sweeper/model/field_test.clj
+++ test/com/mine_sweeper/model/field_test.clj
@@ -1,10 +1,14 @@
 (ns com.mine-sweeper.model.field-test
   (:require [clojure.test :refer :all]
-            [com.mine-sweeper.model.field :refer :all]
+            [com.mine-sweeper.model.field :as field]
             [fulcro-spec.core :refer [=> assertions component behavior specification when-mocking]]))
 
 
-(specification "mine-field" :focus
-  (assertions
-
-  ))
+(specification "build-grid" :focus
+               (assertions
+                 "When number of rows is equal to height"
+                 (count (field/build-grid 2 3)) => 3
+                 "When number of cell is equal to width"
+                 (into #{} (mapv count (field/build-grid 2 3))) => #{2}
+                 "When given negative args, returns a empty vector"
+                 (field/build-grid -1 -3) => []))
