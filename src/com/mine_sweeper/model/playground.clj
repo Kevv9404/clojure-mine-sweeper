57e1556b75863764d752c82fc7a6f2d700629076
dataicomacbook8 <davidjimenezch20@gmail.com>
2025-03-03 16:31:04 -0500

    draw grid - first condition

--- src/com/mine_sweeper/model/playground.clj
+++ src/com/mine_sweeper/model/playground.clj
@@ -7,9 +7,20 @@
 (defn draw-cell [^Terminal t grid cell]
   )
 
+(defn setup [width height n]
+  (-> (field/mine-field width height)
+      (field/populate-mines n)
+      (field/initialize-mine-counts))
+  )
 
-(defn draw-grid [^Terminal t mine-field ]
+(defn draw-grid [^Terminal t mine-field]
   (let [grid (:mine-field/grid mine-field)]
+    (mapv (fn [row]
+            (mapv (fn [cell]
+                    (if (= (:cell/hidden? cell) true)
+                      (terminal/put-character t (:cell/x cell) (:cell/y cell) \X)))
+                  row))
+          grid)
     (doseq [y (range (count grid))
             x (range (count (first grid)))]
       (draw-cell t grid (get-in grid [x y])))))
