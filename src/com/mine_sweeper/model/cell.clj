36322f8ee5367ceadd17617c6318d53273ee7610
Tony Kay <tony.kay@gmail.com>
2025-03-04 06:37:15 -0800

    refactoring

--- src/com/mine_sweeper/model/cell.clj
+++ src/com/mine_sweeper/model/cell.clj
@@ -1,1 +1,13 @@
 (ns com.mine-sweeper.model.cell)
+
+(defn empty-cell []
+  {:cell/hidden?  true
+   :cell/content  0
+   :cell/flagged? false})
+
+(defn mined? [{:cell/keys [content]}] (= content :mine))
+(def not-mined? (complement mined?))
+(defn set-adjacent-content [c n] (assoc c :cell/content n))
+(defn set-mine [c] (assoc c :cell/content :mine))
+(defn set-flag [c] (assoc c :cell/flagged true))
+(defn content [c] (:cell/content c))
--- src/com/mine_sweeper/model/field.clj
+++ src/com/mine_sweeper/model/field.clj
@@ -1,15 +1,15 @@
 (ns com.mine-sweeper.model.field
-  (:require [com.mine-sweeper.model.grid :as utils]))
+  (:require [com.mine-sweeper.model.cell :as cell]
+            [com.mine-sweeper.model.grid :as utils]))
 
-(defn empty-cell [] {:cell/hidden?  true
-                     :cell/flagged? false})
+(defn mine-field [w h]
+  {:mine-field/grid (utils/vectors-of cell/empty-cell w h)})
 
-(defn mine-field [w h] {:mine-field/grid (utils/vectors-of empty-cell w h)})
-(defn mined? [{:cell/keys [content]}] (= content :mine))
-(def not-mined? (complement mined?))
+(defn set-field-content [mine-field x y value]
+  (assoc-in mine-field [:mine-field/grid x y :cell/content] value))
 
-(defn set-field-content [mine-field x y value] (assoc-in mine-field [:mine-field/grid x y :cell/content] value))
-(defn get-field-content [mine-field x y] (get-in mine-field [:mine-field/grid x y :cell/content]))
+(defn get-field-content [mine-field x y]
+  (get-in mine-field [:mine-field/grid x y :cell/content]))
 
 (defn populate-mines
   "Returns an updated mine field with n mines in random locations. NOTE: The grid must be empty if you want to ensure n new mines."
@@ -17,37 +17,19 @@
   (let [locations (utils/random-grid-locations width height n)]
     (reduce (fn [m [x y]] (set-field-content m x y :mine)) mf locations)))
 
+(defn expose-all-cells
+  "Returns an updated mine field with all cells exposed."
+  [{:mine-field/keys [width height] :as mine-field}]
+  (let [cells (utils/grid-locations width height)]
+    (reduce (fn [m [x y]] (assoc-in m [:mine-field/grid x y :cell/hidden?] false)) mine-field cells)))
+
 (defn expand [{:keys [grid] :as mine-field} x y]
-  (let [cell                     (get-in mine-field [x y])
-        adjacent-not-mined-cells (into #{} (filter not-mined? (adjacent-cells mine-field x y)))]
+  (let [cell (get-in mine-field [x y])]
     (cond
-      (empty? (:cell/content cell))
-      (mapv (fn [row] (mapv (fn [c]
-                              (if (or (= c cell) (contains? c adjacent-not-mined-cells))
-                                (assoc c :cell/hidden? false))) row)) grid)
-      (mined? cell)
-      (mapv (fn [row] (mapv (fn [cell] (assoc cell :cell/hidden? false)) row)) grid))))
+      (= 0 (:cell/content cell))
+      (let [adjacent-cells (utils/adjacent-cells grid x y)]
+        (reduce (fn [m [x y]] (expand m x y)) mine-field adjacent-cells))
 
-(defn set-adjacent-content [c n] (assoc c :cell/content n))
+      (cell/mined? cell) (expose-all-cells mine-field)
 
-(defn set-mine [c] (assoc c :cell/content :mine))
-
-(defn set-flag [c] (assoc c :cell/flagged true))
-
-
-(comment
-  ;; 2 random number generators:
-  ;;   * a bit of hardware is used to gather "entropy" and generate a number as a result. These are more "secure"
-  ;;      * Used for cryptography...
-  ;;   * Appearance of "randomness". Math sequence, based on an INITIAL seed, which generates new numbers BASED on the seed, which
-  ;;     basically changes to the prior random number generated.
-  ;;      * The probability of seeing a number is 1/n where n is the range
-  ;;      * The probability of seeing a number twice in a row is 1/n + 1/n where n is the range
-  (count (random-grid-locations 20 10 10))
-  (mine-field 2 2)
-  (let [r (java.util.Random. 1453361670)]
-    (repeatedly 10 #(.nextInt r)))
-  (get-cell-at-coordinate (mine-field 3 3) 2 1)
-  (adjacent-cells (mine-field 6 6) 0 0)
-  (build-grid 2 -1)
-  )
+      :else mine-field)))
