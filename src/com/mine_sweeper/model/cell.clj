3742eea012b0566c8e1d5fd422ee7fc64af6c03a
kevinarroyoc <karroyocardenas@gmail.com>
2025-03-05 09:47:11 -0500

    Playground

--- src/com/mine_sweeper/model/cell.clj
+++ src/com/mine_sweeper/model/cell.clj
@@ -10,3 +10,8 @@
 (defn set-mine [c] (assoc c :cell/content :mine))
 
 (defn set-flag [c] (assoc c :cell/flagged true))
+
+(defn exposed? [cell] (not (:cell/hidden? cell)))
+
+(defn exposed-mine? [cell] (and (exposed? cell) (mined? cell)))
+
