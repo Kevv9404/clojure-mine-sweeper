7afad2f37ada5c9f7cf5bbc14ef75b392628b9e9
Tony Kay <tony.kay@gmail.com>
2025-03-13 10:52:54 -0700

    Added css. Added auto-rendering tweaks.

--- src/com/mine_sweeper/main.cljs
+++ src/com/mine_sweeper/main.cljs
@@ -5,32 +5,36 @@
     [com.mine-sweeper.logic :as logic]))
 
 (defonce root (atom nil))
-(defonce game-state (atom (logic/setup 10 10 10)))
 
-(defn render-mine-field [{:mine-field/keys [width height grid] :as mine-field}]
-  (dom/div
-    (mapv
-      (fn [row]
-        (dom/div {:style {:display :inline}}
-          (mapv
-            (fn [cell]
-              (dom/div {:onClick (fn []
-                                   (println "Clicked on " cell))
-                        :style {:display "inline"
-                                :width "40px"}} (str (:cell/content cell))))
-            row)))
-      grid)))
+(declare render-mine-field game-state)
 
-#_(defn draw! [^Terminal t {:mine-field/keys [width height grid] :as mine-field}]
-    (terminal/clear! t)
-    (when (logic/game-over? mine-field)
-      (display-game-over-message t width height))
-    (let [[cx cy] (:cursor-position mine-field)]
-      (doseq [y (range height)]
-        (doseq [x (range width)]
-          (terminal/put-character t x y (first (cell/cell-character (get-in grid [x y]))))))
-      (terminal/move-cursor t cx cy)
-      (terminal/flush! t)))
+(defn render! []
+  (let [ui (render-mine-field @game-state)]
+    (.render @root ui)))
+
+(defonce game-state (let [a (atom (logic/setup 10 10 10))]
+                      (add-watch a :render render!)
+                      a))
+
+(defn render-mine-field [{:keys [cursor-position]
+                          :mine-field/keys [width height grid] :as mine-field}]
+  (dom/div {:tabIndex  0
+            :onKeyDown (fn [evt]
+                         (swap! game-state identity)
+                         (println (.-key evt)))}
+    (dom/div
+      (mapv
+        (fn [row]
+          (dom/div {:style {:display :inline}}
+            (mapv
+              (fn [cell]
+                (dom/div {:onClick (fn []
+                                     (println "Clicked on " cell))
+                          :style   {:display "inline"
+                                    :width   "40px"}} (str (:cell/content cell))))
+              row)))
+        grid))))
+
 (defn init []
   (println "Initializing app")
   (let [the-real-div (.getElementById js/document "app")]
