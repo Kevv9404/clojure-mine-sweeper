c3a300080db3ea2810f7edea36527ed9fac2569b
Isabel <shakiraisabelgp1003@gmail.com>
2025-03-20 11:53:30 -0500

    Mutations and resolvers

--- src/com/example/example1.cljs
+++ src/com/example/example1.cljs
@@ -1,6 +1,7 @@
 (ns com.example.example1
   (:require
     [clojure.core.async :as async]
+    [com.example.button :as button]
     [com.fulcrologic.fulcro.algorithms.data-targeting :as target]
     [com.fulcrologic.fulcro.algorithms.merge :as merge]
     [com.fulcrologic.fulcro.algorithms.tempid :as tempid]
@@ -8,46 +9,30 @@
     [com.fulcrologic.fulcro.application :as app]
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
     [com.fulcrologic.fulcro.data-fetch :as df]
+    [com.example.parser :as parser]
     [com.fulcrologic.fulcro.dom :as dom]
+    [com.fulcrologic.fulcro.dom.events :as evt]
     [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
     [com.fulcrologic.fulcro.react.version18 :as v18]
     [edn-query-language.core :as eql]
     [fulcro.inspect.tool :refer [add-fulcro-inspect!]]))
 
-(defonce next-id (atom 100))
-(defonce buttons-on-server (atom [{:button/id 1 :button/label "A" :button/color "black"}
-                                  {:button/id 2 :button/label "B" :button/color "black"}]))
 
 (defn handle-remote-interaction [this {::txn/keys [ast result-handler] :as request}]
   (async/go
     (async/<! (async/timeout 200))
     (let [txn (eql/ast->query ast)
-          {:keys [key params]} (-> ast :children first)]
-      (cond
-        (= :buttons key)
-        (result-handler
+          response (parser/parser {} txn)
+          {:keys [key]} (-> ast :children first)
+          response-status-fail (keyword? (get response key))]
+      (result-handler
+        (if response-status-fail
+          {:transaction txn
+           :status-code 400
+           :body        response}
           {:transaction txn
            :status-code 200
-           :body        {:buttons @buttons-on-server}})
-
-        (and (symbol? key) (= "add-button" (name key)))
-        (let [new-button  (merge params {:button/color "black" :button/border "green"})
-              incoming-id (:button/id params)
-              real-id     (swap! next-id inc)
-              new-button  (assoc new-button :button/id real-id)]
-          (println new-button)
-          (swap! buttons-on-server conj new-button)
-          (result-handler
-            {:transaction txn
-             :status-code 200
-             :body        {key (assoc new-button
-                                 :tempids {incoming-id real-id})}}))
-
-        :else
-        (result-handler
-          {:transaction txn
-           :status-code 200
-           :body        {}})))))
+           :body        response})))))
 
 (defonce app (v18/with-react18
                (app/fulcro-app
@@ -61,19 +46,18 @@
 
 (defmutation increment-button [_]
   (action [{:keys [state ref]}]
-    (swap! state update-in ref counter-button-click)))
+          (swap! state update-in ref counter-button-click)))
 
 (declare CounterButton)
 
 (defmutation add-button [{:button/keys [id label] :as button}]
   (action [{:keys [state]}]                                 ; OPTIMISTIC UPDATE
-    (swap! state
-      merge/merge-component CounterButton button
-      :append [:buttons]))
-  (remote [env]
-    (-> env
-      (m/returning CounterButton)
-      (m/with-target (target/append-to [:buttons])))))
+          (swap! state
+                 merge/merge-component CounterButton button
+                 :append [:component/id :top-container :buttons]))
+  (remote [env] true #_(-> env
+                           (m/returning CounterButton)
+                           (m/with-target (target/append-to [:component/id :top-container :buttons])))))
 
 (defsc CounterButton [this {:ui/keys     [clicks error-message]
                             :button/keys [id label color] :as props}]
@@ -85,32 +69,48 @@
                    :button/color "blue"
                    :ui/clicks    (or :param/start-at 0)}}
   (dom/li nil
-    (if error-message
-      (dom/div error-message)
-      (dom/button {:style   {:backgroundColor color
-                             :color           "white"}
-                   :onClick (fn [] (comp/transact!! this [(increment-button)]))}
-        (str label " w/clicks " clicks)))))
+          (if error-message
+            (dom/div error-message)
+            (dom/button {:style   {:backgroundColor color
+                                   :color           "white"}
+                         :onClick (fn [] (comp/transact!! this [(increment-button)]))}
+                        (str label " w/clicks " clicks)))))
 
 (def ui-counter-button (comp/factory CounterButton {:keyfn :button/id}))
 
-(defsc Root [this {:keys [buttons]}]
-  {:query         [{:buttons (comp/get-query CounterButton)}]
+(defsc TopContainer [this {:keys    [buttons]
+                           :ui/keys [new-b-color new-b-label]}]
+  {:query         [{:buttons (comp/get-query CounterButton)}
+                   :ui/new-b-color :ui/new-b-label]
+   :ident         (fn [] [:component/id :top-container])
    :initial-state {:buttons []}}
   (dom/div nil
-    (dom/button
-      {:onClick (fn [] (comp/transact! this [(add-button {:button/id    (tempid/tempid)
-                                                          :button/color "green"
-                                                          :button/label "Boo"})]))}
-      "Add a button")
-    (dom/h2 "My Buttons")
-    (dom/ul nil
-      (mapv ui-counter-button buttons))))
+           (dom/input {:value       (str new-b-color)
+                       :placeholder "Insert color"
+                       :onChange    #(m/set-value! this :ui/new-b-color (evt/target-value %))})
+           (dom/input {:value       (str new-b-label)
+                       :onChange    #(m/set-string! this :ui/new-b-label :event %)
+                       :placeholder "Insert label"})
+           (dom/button
+             {:onClick (fn [] (comp/transact! this [(add-button {:button/id    (tempid/tempid)
+                                                                 :button/color (or new-b-color "black")
+                                                                 :button/label (or new-b-label "default")})]))}
+             "Add a button")
+           (dom/h2 "My Buttons")
+           (dom/ul nil
+                   (mapv ui-counter-button buttons))))
+(def ui-top-container (comp/factory TopContainer))
+
+(defsc Root [this {:keys [top-container]}]
+  {:query         [{:top-container (comp/get-query TopContainer)}]
+   :initial-state {:top-container {}}}
+  (ui-top-container top-container))
 
 (defn refresh []
   (app/mount! app Root "app"))
 
 (defn init []
+  (button/load-db!)
   (refresh)
   (add-fulcro-inspect! app)
-  (df/load! app :buttons CounterButton))
+  (df/load! app :buttons CounterButton {:target [:component/id :top-container :buttons]}))
---
+++ src/com/example/parser.cljs
@@ -0,0 +1,18 @@
+(ns com.example.parser
+  (:require
+    [com.wsscode.pathom.connect :as pc]
+    [com.example.button :as button]
+    [com.wsscode.pathom.core :as p]))
+
+(def all-resolvers [button/resolvers])
+
+(def parser
+  (p/parser {::p/env     {::p/reader               [p/map-reader
+                                                    pc/reader2
+                                                    pc/open-ident-reader
+                                                    p/env-placeholder-reader]
+                          ::p/placeholder-prefixes #{">"}}
+             ::p/mutate  pc/mutate
+             ::p/plugins [(pc/connect-plugin {::pc/register all-resolvers})
+                          p/trace-plugin]}))
+
